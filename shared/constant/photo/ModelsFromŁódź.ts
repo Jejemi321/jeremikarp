ddc69777f2f21c9b446b4b42ae99cce665ed5842
Jeremi Karp <jeremikarp@gmail.com>
2025-11-17 00:30:52 +0100

    20

--- shared/constant/photo/ModelsFromŁódź.ts
+++ shared/constant/photo/ModelsFromŁódź.ts
@@ -3,14 +3,14 @@ import { PhotoPortfolioItemsType } from "../PhotoPortfolioItems";
 export const ModelsFromŁódź: PhotoPortfolioItemsType = {
 	title: "ModelsFromŁódźTitle",
 	description: "ModelsFromŁódźDescription",
-	imageCover: "/assets/img/portfolio/photo/ModelsFromŁódź/05.webp",
+	imageCover: "/assets/img/portfolio/photo/ModelsFromLodz/05.webp",
 	date: "2023-03-18",
 	favorite: false,
 	category: "CategoryPhotography",
 	imgArray: Array.from({ length: 15 }, (_, i) => ({
 		id: i + 1,
 		title: `Models from Łódź - ${String(i + 1).padStart(2, "0")}`,
-		src: `/assets/img/portfolio/photo/ModelsFromŁódź/${String(i + 1).padStart(
+		src: `/assets/img/portfolio/photo/ModelsFromLodz/${String(i + 1).padStart(
 			2,
 			"0"
 		)}.webp`,
